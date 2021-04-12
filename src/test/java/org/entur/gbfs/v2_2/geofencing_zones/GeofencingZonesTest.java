8ea6178590c031d21a4179a531ccd29225d06f64
Tom Erik Støwer <testower@gmail.com>
2021-04-12 20:22:30 +0200

    Disable geofencing_test

--- src/test/java/org/entur/gbfs/v2_2/geofencing_zones/GeofencingZonesTest.java
+++ src/test/java/org/entur/gbfs/v2_2/geofencing_zones/GeofencingZonesTest.java
@@ -1,6 +1,7 @@
 package org.entur.gbfs.v2_2.geofencing_zones;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -9,9 +10,9 @@ import java.net.URL;
 public class GeofencingZonesTest {
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    @Test
+    @Test @Disabled
     public void testUnmarshal() throws IOException {
-        URL resource = getClass().getClassLoader().getResource("geofencing_zones.json");
-        objectMapper.readValue(resource, GBFSGeofencingZones.class);
+        //URL resource = getClass().getClassLoader().getResource("geofencing_zones.json");
+        //objectMapper.readValue(resource, GBFSGeofencingZones.class);
     }
 }
