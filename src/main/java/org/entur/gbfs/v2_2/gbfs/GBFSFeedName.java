7d78252f0a87a6a14a0d95d45bb35b1eaf9c4b44
Hannes Junnila <hannes.junnila@gmail.com>
2021-09-13 13:49:47 +0300

    Add reference to implementing class for each feed

--- src/main/java/org/entur/gbfs/v2_2/gbfs/GBFSFeedName.java
+++ src/main/java/org/entur/gbfs/v2_2/gbfs/GBFSFeedName.java
@@ -2,36 +2,52 @@ package org.entur.gbfs.v2_2.gbfs;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
+import org.entur.gbfs.v2_2.free_bike_status.GBFSFreeBikeStatus;
+import org.entur.gbfs.v2_2.gbfs_versions.GBFSGbfsVersions;
+import org.entur.gbfs.v2_2.geofencing_zones.GBFSGeofencingZones;
+import org.entur.gbfs.v2_2.station_information.GBFSStationInformation;
+import org.entur.gbfs.v2_2.station_status.GBFSStationStatus;
+import org.entur.gbfs.v2_2.system_alerts.GBFSSystemAlerts;
+import org.entur.gbfs.v2_2.system_calendar.GBFSSystemCalendar;
+import org.entur.gbfs.v2_2.system_hours.GBFSSystemHours;
+import org.entur.gbfs.v2_2.system_information.GBFSSystemInformation;
+import org.entur.gbfs.v2_2.system_pricing_plans.GBFSSystemPricingPlans;
+import org.entur.gbfs.v2_2.system_regions.GBFSSystemRegions;
+import org.entur.gbfs.v2_2.vehicle_types.GBFSVehicleTypes;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public enum GBFSFeedName {
-    GBFS("gbfs"),
-    GBFSVersions("gbfs_versions"),
-    SystemInformation("system_information"),
-    VehicleTypes("vehicle_types"),
-    StationInformation("station_information"),
-    StationStatus("station_status"),
-    FreeBikeStatus("free_bike_status"),
-    SystemHours("system_hours"),
-    SystemAlerts("system_alerts"),
-    SystemCalendar("system_calendar"),
-    SystemRegions("system_regions"),
-    SystemPricingPlans("system_pricing_plans"),
-    GeofencingZones("geofencing_zones");
+    GBFS("gbfs", GBFS.class),
+    GBFSVersions("gbfs_versions", GBFSGbfsVersions.class),
+    SystemInformation("system_information", GBFSSystemInformation.class),
+    VehicleTypes("vehicle_types", GBFSVehicleTypes.class),
+    StationInformation("station_information", GBFSStationInformation.class),
+    StationStatus("station_status", GBFSStationStatus.class),
+    FreeBikeStatus("free_bike_status", GBFSFreeBikeStatus.class),
+    SystemHours("system_hours", GBFSSystemHours.class),
+    SystemAlerts("system_alerts", GBFSSystemAlerts.class),
+    SystemCalendar("system_calendar", GBFSSystemCalendar.class),
+    SystemRegions("system_regions", GBFSSystemRegions.class),
+    SystemPricingPlans("system_pricing_plans", GBFSSystemPricingPlans.class),
+    GeofencingZones("geofencing_zones", GBFSGeofencingZones.class);
 
     private final String value;
+    private final Class<?> implementingClass;
     private final static Map<String, GBFSFeedName> CONSTANTS = new HashMap<String, GBFSFeedName>();
+    private final static Map<Class<?>, GBFSFeedName> CLASSES = new HashMap<Class<?>, GBFSFeedName>();
 
     static {
         for (GBFSFeedName c: values()) {
             CONSTANTS.put(c.value, c);
+            CLASSES.put(c.implementingClass, c);
         }
     }
 
-    private GBFSFeedName(String value) {
+    private GBFSFeedName(String value, Class<?> implementingClass) {
         this.value = value;
+        this.implementingClass = implementingClass;
     }
 
     @Override
@@ -44,6 +60,10 @@ public enum GBFSFeedName {
         return this.value;
     }
 
+    public Class<?> implementingClass() {
+        return implementingClass;
+    }
+
     @JsonCreator
     public static GBFSFeedName fromValue(String value) {
         GBFSFeedName constant = CONSTANTS.get(value);
@@ -54,5 +74,13 @@ public enum GBFSFeedName {
         }
     }
 
+    public static GBFSFeedName fromClass(Class<?> clazz) {
+        GBFSFeedName constant = CLASSES.get(clazz);
+        if (constant == null) {
+            throw new IllegalArgumentException(clazz.getName());
+        } else {
+            return constant;
+        }
+    }
 }
 
