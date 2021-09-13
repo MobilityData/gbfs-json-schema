0c28e30401983fae2ca7d2cc39ae44c2fb0e9504
Hannes Junnila <hannes.junnila@gmail.com>
2021-09-13 14:30:47 +0300

    Add test for GBFSFeedName

---
+++ src/test/java/org/entur/gbfs/v2_2/gbfs/GBFSFeedNameTest.java
@@ -0,0 +1,29 @@
+package org.entur.gbfs.v2_2.gbfs;
+
+import org.entur.gbfs.v2_2.station_information.GBFSStationInformation;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GBFSFeedNameTest {
+
+    @Test
+    void value() {
+        assertEquals("station_information", GBFSFeedName.StationInformation.value());
+    }
+
+    @Test
+    void implementingClass() {
+        assertEquals(GBFSStationInformation.class, GBFSFeedName.StationInformation.implementingClass());
+    }
+
+    @Test
+    void fromValue() {
+        assertEquals(GBFSFeedName.StationInformation, GBFSFeedName.fromValue("station_information"));
+    }
+
+    @Test
+    void fromClass() {
+        assertEquals(GBFSFeedName.StationInformation, GBFSFeedName.fromClass(GBFSStationInformation.class));
+    }
+}
